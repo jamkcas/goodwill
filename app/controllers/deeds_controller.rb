47164351ab304e27c3cb855c36128b6a7a51e544
james <jamkcas@yahoo.com>
2013-12-08 16:34:24 -0800

    added facebook login and user model

---
+++ app/controllers/deeds_controller.rb
@@ -0,0 +1,4 @@
+class DeedsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 GoodwillTracker::Application.routes.draw do
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
 
+  root to: 'deeds#index'
+  resources :deeds
 end
