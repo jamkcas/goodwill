7dba6e41a74c867218b2b217d2532741e814774f
james <jamkcas@yahoo.com>
2013-12-17 09:10:13 -0800

    added config.assets.initialize_on_precompile = false to application.rb

--- config/application.rb
+++ config/application.rb
@@ -69,6 +69,6 @@ module GoodwillTracker
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.action_mailer.default_url_options = { host: 'localhost:3000' }
+    config.assets.initialize_on_precompile = false
   end
 end
